f630bc02bc6929e3427137869b3922e2afe1b749
boi <163528419+bois3n@users.noreply.github.com>
2024-04-11 18:48:41 +0200

    Keycloak implemented

---
+++ src/security/keycloak/keycloak.module.ts
@@ -0,0 +1,41 @@
+// eslint-disable-next-line max-classes-per-file
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  RoleGuard,
+} from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
+import { KeycloakService } from './keycloak.service.js';
+import { LoginController } from './login.controller.js';
+import { LoginResolver } from './login.resolver.js';
+import { Module } from '@nestjs/common';
+
+@Module({
+  providers: [KeycloakService],
+  exports: [KeycloakService],
+})
+class ConfigModule {}
+
+@Module({
+  imports: [
+    KeycloakConnectModule.registerAsync({
+      useExisting: KeycloakService,
+      imports: [ConfigModule],
+    }),
+  ],
+  controllers: [LoginController],
+  providers: [
+    KeycloakService,
+    LoginResolver,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
+  exports: [KeycloakConnectModule],
+})
+export class KeycloakModule {}
