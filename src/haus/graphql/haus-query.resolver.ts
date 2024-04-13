41d5a0a64da3d344c876d297077a9020e92fba63
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-04-13 09:32:02 +0200

    Fixed logger import issue

--- src/haus/graphql/haus-query.resolver.ts
+++ src/haus/graphql/haus-query.resolver.ts
@@ -4,54 +4,54 @@ import { Haus } from '../entity/haus.entity';
 import { Suchkriterien } from './suchkriterien';
 import { HausReadService } from '../service/haus-read.service.js';
 import { HttpExceptionFilter } from './http-exception.filter';
-import { ResponseTimeInterceptor } from 'src/logger/response-time.interceptor';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor';
 import { Public } from 'nest-keycloak-connect';
-import { getLogger } from 'src/logger/logger';
+import { getLogger } from '../../logger/logger';
 
 export interface IdInput {
-  readonly id: number;
+    readonly id: number;
 }
 
 export interface SuchkriterienInput {
-  readonly suchkriterien: Suchkriterien;
+    readonly suchkriterien: Suchkriterien;
 }
 
 @Resolver((_: any) => Haus)
 @UseFilters(HttpExceptionFilter)
 @UseInterceptors(ResponseTimeInterceptor)
 export class HausQueryResolver {
-  readonly #service: HausReadService;
-  readonly #logger = getLogger(HausQueryResolver.name);
+    readonly #service: HausReadService;
+    readonly #logger = getLogger(HausQueryResolver.name);
 
-  constructor(service: HausReadService) {
-    this.#service = service;
-  }
-
-  @Query('haus')
-  @Public()
-  async findById(@Args('id') id: IdInput) {
-    this.#logger.debug('findById(%o)', id);
-
-    const haus = await this.#service.findById(id);
-
-    if (this.#logger.isLevelEnabled('debug')) {
-      this.#logger.debug('findById(%o) = %o', haus.toString());
+    constructor(service: HausReadService) {
+        this.#service = service;
     }
 
-    return haus;
-  }
+    @Query('haus')
+    @Public()
+    async findById(@Args('id') id: IdInput) {
+        this.#logger.debug('findById(%o)', id);
 
-  @Query('haeuser')
-  @Public()
-  async find(@Args() input: SuchkriterienInput | undefined) {
-    this.#logger.debug('find: input=%o', input);
+        const haus = await this.#service.findById(id);
 
-    const haeuser = await this.#service.find(input?.suchkriterien);
+        if (this.#logger.isLevelEnabled('debug')) {
+            this.#logger.debug('findById(%o) = %o', haus.toString());
+        }
 
-    if (this.#logger.isLevelEnabled('debug')) {
-      this.#logger.debug('find(%o) = %o', haeuser);
+        return haus;
     }
 
-    return haeuser;
-  }
+    @Query('haeuser')
+    @Public()
+    async find(@Args() input: SuchkriterienInput | undefined) {
+        this.#logger.debug('find: input=%o', input);
+
+        const haeuser = await this.#service.find(input?.suchkriterien);
+
+        if (this.#logger.isLevelEnabled('debug')) {
+            this.#logger.debug('find(%o) = %o', haeuser);
+        }
+
+        return haeuser;
+    }
 }
